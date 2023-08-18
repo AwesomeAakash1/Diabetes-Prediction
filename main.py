260249a42205a4bd30542175cf12daf05f0b7e52
Aakash Kolli <104699429+AwesomeAakash1@users.noreply.github.com>
2023-08-18 17:51:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,195 @@
+# Imports
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns # for data visualization
+import matplotlib.pyplot as plt # to plot charts
+from collections import Counter
+import numpy as np
+import os
+
+# Libraries for ML model
+from sklearn.preprocessing import QuantileTransformer
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold, learning_curve, train_test_split
+
+# Pima Indians Diabetes Dataset, sourced from Kaggle
+df = pd.read_csv("diabetes.csv")
+
+# Preview dataset before cleaning
+
+# df.info() 
+# df.head()
+# df.describe()
+
+# No missing values
+# df.isnull().sum()
+
+# Data Cleaning
+# replace 0s with means of cols
+df['Glucose'] = df['Glucose'].replace(0, df['Glucose'].mean())
+# Correcting missing values in blood pressure
+df['BloodPressure'] = df['BloodPressure'].replace(0, df['BloodPressure'].mean()) # There are 35 records with 0 BloodPressure in dataset
+# Correcting missing values in BMI
+df['BMI'] = df['BMI'].replace(0, df['BMI'].median())
+# Correct missing values in Insulin and SkinThickness
+
+df['SkinThickness'] = df['SkinThickness'].replace(0, df['SkinThickness'].median())
+df['Insulin'] = df['Insulin'].replace(0, df['Insulin'].median())
+
+# Cleaned dataest
+# df.describe()
+
+# Data visualizations
+plt.figure(figsize=(13,10))
+sns.heatmap(df.corr(),annot=True, fmt = ".2f", cmap = "coolwarm")
+
+# Explore Pregnancies vs Outcome
+plt.figure(figsize=(13,6))
+g = sns.kdeplot(df["Pregnancies"][df["Outcome"] == 1], 
+     color="Red", shade = True)
+g = sns.kdeplot(df["Pregnancies"][df["Outcome"] == 0], 
+     ax =g, color="Green", shade= True)
+g.set_xlabel("Pregnancies")
+g.set_ylabel("Frequency")
+g.legend(["Positive","Negative"])
+
+# View individuals with and without diabetes, for context
+plt.figure(figsize=(10, 6))
+sns.countplot(data=df, x='Outcome')
+plt.title('Distribution of Outcome')
+plt.show()
+
+# Explore Glucose vs Outcome
+plt.figure(figsize=(10,6))
+sns.violinplot(data=df, x="Outcome", y="Glucose",
+               split=True, inner="quart", linewidth=1)
+
+# Explore Glucose vs Outcome
+plt.figure(figsize=(13,6))
+g = sns.kdeplot(df["Glucose"][df["Outcome"] == 1], color="Red", shade = True)
+g = sns.kdeplot(df["Glucose"][df["Outcome"] == 0], ax =g, color="Green", shade= True)
+g.set_xlabel("Glucose")
+g.set_ylabel("Frequency")
+g.legend(["Positive","Negative"])
+
+# Glucose vs BMI vs Age
+
+plt.figure(figsize=(20,10))
+sns.scatterplot(data=df, x="Glucose", y="BMI", hue="Age", size="Age")
+
+# Feature Engineering (to improve performance)
+# Outlier Detection
+
+def detect_outliers(df,n,features):
+    outlier_indices = []
+    """
+    Detect outliers from given list of features. It returns a list of the indices
+    according to the observations containing more than n outliers according
+    to the Tukey method
+    """
+    # iterate over features(columns)
+    for col in features:
+        Q1 = np.percentile(df[col], 25)
+        Q3 = np.percentile(df[col],75)
+        IQR = Q3 - Q1
+        
+        # outlier step
+        outlier_step = 1.5 * IQR
+        
+        # Determine a list of indices of outliers for feature col
+        outlier_list_col = df[(df[col] < Q1 - outlier_step) | (df[col] > Q3 + outlier_step )].index
+        
+        # append the found outlier indices for col to the list of outlier indices 
+        outlier_indices.extend(outlier_list_col)
+        
+    # select observations containing more than 2 outliers
+    outlier_indices = Counter(outlier_indices)
+    multiple_outliers = list( k for k, v in outlier_indices.items() if v > n )
+    
+    return multiple_outliers   
+
+# detect outliers from numeric features
+outliers_to_drop = detect_outliers(df, 2 ,["Pregnancies", 'Glucose', 'BloodPressure', 'BMI', 'DiabetesPedigreeFunction', 'SkinThickness', 'Insulin', 'Age'])
+
+# remove outliers from dataset
+df.drop(df.loc[outliers_to_drop].index, inplace=True)
+
+# Modeling
+# Transform data into quantile (Normal dist.)
+q  = QuantileTransformer()
+X = q.fit_transform(df)
+transformedDF = q.transform(X)
+transformedDF = pd.DataFrame(X)
+transformedDF.columns =['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age', 'Outcome']
+transformedDF.head()
+
+# Split data for training and testing
+features = df.drop(["Outcome"], axis=1)
+labels = df["Outcome"]
+x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.30, random_state=7)
+
+# Cross Validation (overfitting detection)
+def evaluate_model(models):
+    """
+    Takes a list of models and returns chart of cross validation scores using mean accuracy
+    """
+    
+    # Cross validate model with Kfold stratified cross val
+    kfold = StratifiedKFold(n_splits = 10)
+    
+    result = []
+    for model in models :
+        result.append(cross_val_score(estimator = model, X = x_train, y = y_train, scoring = "accuracy", cv = kfold, n_jobs=4))
+
+    cv_means = []
+    cv_std = []
+    for cv_result in result:
+        cv_means.append(cv_result.mean())
+        cv_std.append(cv_result.std())
+
+    result_df = pd.DataFrame({
+        "CrossValMeans":cv_means,
+        "CrossValerrors": cv_std,
+        "Models":[
+            "LogisticRegression",
+            "DecisionTreeClassifier",
+            "AdaBoostClassifier",
+            "SVC",
+            "RandomForestClassifier",
+            "GradientBoostingClassifier",
+            "KNeighborsClassifier"
+        ]
+    })
+
+    # Generate chart
+    bar = sns.barplot(x = "CrossValMeans", y = "Models", data = result_df, orient = "h")
+    bar.set_xlabel("Mean Accuracy")
+    bar.set_title("Cross validation scores")
+    return result_df
+
+# Using different models with controlled randomization
+random_state = 30
+models = [
+    LogisticRegression(random_state = random_state, solver='liblinear'),
+    DecisionTreeClassifier(random_state = random_state),
+    AdaBoostClassifier(DecisionTreeClassifier(random_state = random_state), random_state = random_state, learning_rate = 0.2),
+    SVC(random_state = random_state),
+    RandomForestClassifier(random_state = random_state),
+    GradientBoostingClassifier(random_state = random_state),
+    KNeighborsClassifier(),
+]
+evaluate_model(models)
+
+result_df = evaluate_model(models)
+
+# Print accuracy scores for each model
+for index, row in result_df.iterrows():
+    print(f"Model: {row['Models']}, Accuracy: {row['CrossValMeans']:.4f} +/- {row['CrossValerrors']:.4f}")
+
+# Identify the model with the highest accuracy
+highest_accuracy_model = result_df.loc[result_df['CrossValMeans'].idxmax()]
+print(f"\nModel with the highest accuracy: {highest_accuracy_model['Models']} (Accuracy: {highest_accuracy_model['CrossValMeans']:.4f} ± {highest_accuracy_model['CrossValerrors']:.4f})")
